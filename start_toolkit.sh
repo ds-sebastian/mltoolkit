484c926d6ec672e171101d86564bc959a8abcafb
Sebastian G <sebastian@gbthome.com>
2023-06-06 22:17:55 -0400

    remove aim

--- start_toolkit.sh
+++ start_toolkit.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Create necessary directories if they do not exist
-mkdir -p ${BACKEND_STORE_URI} ${DEFAULT_ARTIFACT_ROOT}
-
 # Start the MLflow server
 mlflow server \
     --host 0.0.0.0 \
@@ -11,18 +8,18 @@ mlflow server \
     --default-artifact-root ${DEFAULT_ARTIFACT_ROOT} & 
 
 # Initialize Aim
-if [ ! -d ".aim" ]; then 
-    aim init; 
-fi
+# if [ ! -d ".aim" ]; then 
+#     aim init; 
+# fi
 
 # Start the Aim server
-aim server --host 127.0.0.1 --port $AIM_SERVER_PORT &
+# aim server --host 127.0.0.1 --port $AIM_SERVER_PORT &
 
 # Start Aim UI
-aim up --host 127.0.0.1 --port $AIM_UI_PORT & 
+# aim up --host 127.0.0.1 --port $AIM_UI_PORT & 
 
 # Sync Aim with MLflow
-aimlflow sync --mlflow-tracking-uri=http://0.0.0.0:$MLFLOW_PORT --aim-repo=/home/${NB_USER}/aim &
+# aimlflow sync --mlflow-tracking-uri=http://0.0.0.0:$MLFLOW_PORT --aim-repo=/home/${NB_USER}/aim &
 
 # Start Jupyter Notebook using the base image's script
 exec start-notebook.sh
